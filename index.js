4ea366cf08ee372322a9f071bb2958313c879f32
al02659529 <44920146+al02659529@users.noreply.github.com>
2020-07-04 18:06:30 -0600

    added build

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
 const express = require('express');
 const app = express();
 const cors = require('cors')
+
+app.use(express.static('build'))
 app.use(cors());
 app.use(express.json())
+
 
 let notes = [
     {    id: 1,    content: "HTML is easy",    date: "2019-05-30T17:30:31.098Z",    important: true  },
@@ -11,9 +13,9 @@ let notes = [
     {    id: 3,    content: "GET and POST are the most important methods of HTTP protocol",    date: "2019-05-30T19:20:14.298Z",    important: true  }
     ]
 
-app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
-})
+// app.get('/', (req, res) => {
+//     res.send('<h1>Hello World</h1>')
+// })
 
 app.get('/api/notes', (req, res) => {
     res.json(notes)
